81a585aab611380aae56ebeef422991a748fc4e4
Joseph Stewart <joseph.s@nationalschoolproject.com>
2020-03-12 21:47:09 -0700

    Add installer

---
+++ package.lua
@@ -0,0 +1,17 @@
+{
+	name = "turtle-master",
+	author = "PossieTV",
+	version = "0.1.0",
+	files = {
+		"/cobalt%-lib/.*",
+		"/cobalt%-ui/.*",
+		"/custom%-elements/.*",
+		"/tm%-helpers/.*",
+		"/turtle%-master%.lua",
+		"/cobalt"
+	},
+	runCommand = {
+		"turtle-master.lua"
+	},
+	contentBaseUrl = "https://raw.githubusercontent.com/josephdangerstewart/turtle-master/master"
+}
